8985dbc2b7779064bac33636af9157e4da639076
fujita koji <fcreation.net@gmail.com>
2025-03-23 01:21:14 +0900

    feat: ItemSeeder にダミーデータ追加、DatabaseSeeder 順序修正

--- src/database/seeders/ItemSeeder.php
+++ src/database/seeders/ItemSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Item;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ItemSeeder extends Seeder
@@ -14,6 +15,88 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        Item::factory()->count(5)->create();
+        $user = User::first();
+
+        $items = [
+            [
+                'title' => '腕時計',
+                'price' => 15000,
+                'description' => 'スタイリッシュなデザインのメンズ腕時計',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Armani+Mens+Clock.jpg',
+                'condition' => '良好',
+            ],
+            [
+                'title' => 'HDD',
+                'price' => 5000,
+                'description' => '高速で信頼性の高いハードディスク',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/HDD+Hard+Disk.jpg',
+                'condition' => '目立った傷や汚れなし',
+            ],
+            [
+                'title' => '玉ねぎ3束',
+                'price' => 300,
+                'description' => '新鮮な玉ねぎ3束のセット',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/iLoveIMG+d.jpg',
+                'condition' => 'やや傷や汚れあり',
+            ],
+            [
+                'title' => '革靴',
+                'price' => 4000,
+                'description' => 'クラシックなデザインの革靴',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Leather+Shoes+Product+Photo.jpg',
+                'condition' => '状態が悪い',
+            ],
+            [
+                'title' => 'ノートPC',
+                'price' => 45000,
+                'description' => '高性能なノートパソコン',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Living+Room+Laptop.jpg',
+                'condition' => '良好',
+            ],
+            [
+                'title' => 'マイク',
+                'price' => 8000,
+                'description' => '高音質のレコーディング用マイク',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Music+Mic+4632231.jpg',
+                'condition' => '目立った傷や汚れなし',
+            ],
+            [
+                'title' => 'ショルダーバッグ',
+                'price' => 3500,
+                'description' => 'おしゃれなショルダーバッグ',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Purse+fashion+pocket.jpg',
+                'condition' => 'やや傷や汚れあり',
+            ],
+            [
+                'title' => 'タンブラー',
+                'price' => 500,
+                'description' => '使いやすいタンブラー',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Tumbler+souvenir.jpg',
+                'condition' => '状態が悪い',
+            ],
+            [
+                'title' => 'コーヒーミル',
+                'price' => 4000,
+                'description' => '手動のコーヒーミル',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Waitress+with+Coffee+Grinder.jpg',
+                'condition' => '良好',
+            ],
+            [
+                'title' => 'メイクセット',
+                'price' => 2500,
+                'description' => '便利なメイクアップセット',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/%E5%A4%96%E5%87%BA%E3%83%A1%E3%82%A4%E3%82%AF%E3%82%A2%E3%83%83%E3%83%95%E3%82%9A%E3%82%BB%E3%83%83%E3%83%88.jpg',
+                'condition' => '目立った傷や汚れなし',
+            ],
+        ];
+
+        foreach ($items as $item) {
+            Item::create(array_merge($item, [
+                'user_id' => $user->id,
+                'is_sold' => false,
+                'status' => 'available',
+            ]));
+        }
+
     }
 }
